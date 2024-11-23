84e3a8085902bbc00627a4b7944c9f85710f3a96
ash1ra <ash1rawtf@gmail.com>
2024-11-23 19:48:07 +0200

    feat: add nvim settings

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,68 @@
+local ascii_art = {
+    "",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"⠄⠄⠄⠄⢠⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣆⠄⠄⠄",
+	"⠄⠄⣼⢀⣿⣿⣿⣿⣏⡏⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⡆⠄⠄",
+	"⠄⠄⡟⣼⣿⣿⣿⣿⣿⠄⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⣿⣇⢻⣿⣿⣿⣿⠄⠄",
+	"⠄⢰⠃⣿⣿⠿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠙⠿⣿⣿⣿⣿⣿⠄⢿⣿⣿⣿⡄⠄",
+	"⠄⢸⢠⣿⣿⣧⡙⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⡇⠸⣿⡿⣸⡇⠄",
+	"⠄⠈⡆⣿⣿⣿⣿⣦⡙⠳⠄⠄⠄⠄⠄⠄⢀⣠⣤⣀⣈⠙⠃⠄⠿⢇⣿⡇⠄",
+	"⠄⠄⡇⢿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⣠⣶⣿⣿⣿⣿⣿⣿⣷⣆⡀⣼⣿⡇⠄",
+	"⠄⠄⢹⡘⣿⣿⣿⢿⣷⡀⠄⢀⣴⣾⣟⠉⠉⠉⠉⣽⣿⣿⣿⣿⠇⢹⣿⠃⠄",
+	"⠄⠄⠄⢷⡘⢿⣿⣎⢻⣷⠰⣿⣿⣿⣿⣦⣀⣀⣴⣿⣿⣿⠟⢫⡾⢸⡟⠄.",
+	"⠄⠄⠄⠄⠻⣦⡙⠿⣧⠙⢷⠙⠻⠿⢿⡿⠿⠿⠛⠋⠉⠄⠂⠘⠁⠞⠄⠄⠄",
+	"⠄⠄⠄⠄⠄⠈⠙⠑⣠⣤⣴⡖⠄⠿⣋⣉⣉⡁⠄⢾⣦⠄⠄⠄⠄⠄⠄⠄⠄",
+	"",
+}
+
+local center = {
+	{
+		action = "lua require('persistence').load()",
+		desc = " restore session",
+		icon = "",
+		key = "s",
+		key_format = "%s",
+	},
+	{
+		action = "qa",
+		desc = " quit",
+		icon = "",
+		key = "q",
+		key_format = "%s",
+	},
+}
+
+local footer = { "you could not live with your own failure.", "where did that bring you? back to me" }
+
+return {
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "doom",
+			hide = { statusline = true },
+			config = {
+				header = ascii_art,
+				center = center,
+				footer = footer,
+				packages = { enable = false },
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>")
+	end,
+}
