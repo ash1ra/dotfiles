9838033c55f63d0b877ff1380e07750ea82e4817
ash1ra <ash1rawtf@gmail.com>
2025-01-19 13:35:55 +0200

    feat: add nvim colorscheme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
-			options = { theme = "onedark" },
+			options = { theme = "tokyonight" },
 		})
 	end,
 }
