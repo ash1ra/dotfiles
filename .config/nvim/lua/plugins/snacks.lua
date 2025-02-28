37d5bc5e84b60a60a42c83e9fb130ce1593f7d8b
ash1ra <ash1rawtf@gmail.com>
2025-02-28 20:45:08 +0200

    feat: change nvim config

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,46 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+				-- {
+				-- 	section = "terminal",
+				-- 	-- cmd = "chafa ~/Downloads/ayanami.jpg --format symbols --symbols vhalf --size 50x50",
+				-- 	cmd = "ascii-image-converter ~/Downloads/ayanami.jpg -C -c",
+				--                 -- random = 10,
+				--                 pane = 2,
+				--                 indent = 4,
+				-- 	heigth = 30,
+				-- },
+			},
+		},
+		explorer = {
+			enabled = true,
+			layout = {
+				cycle = false,
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>th",
+			function()
+				require("snacks").picker.colorschemes()
+			end,
+			desc = "Colorschemes",
+		},
+		{
+			"<leader>n",
+			function()
+				require("snacks").explorer()
+			end,
+			desc = "File Explorer",
+		},
+	},
+}
