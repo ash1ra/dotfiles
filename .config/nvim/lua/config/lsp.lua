9ee268a0a2f0e68b1aa5026afe726a760b4125ef
ash1ra <ash1rawtf@gmail.com>
2025-09-20 21:35:50 +0300

    feat (nvim): add blink.cmp plugin and some lsps

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,1 +1,2 @@
-vim.lsp.enable({ "lua_ls", "bashls", "ruff" })
+vim.lsp.enable({ "lua_ls", "bashls", "ruff", "ty", "cssls", "cssmodules_ls", "css_variables", "html", })
+vim.diagnostic.config({ virtual_text = true })
--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,8 +1,42 @@
 vim.pack.add({
 	{ src = "https://github.com/metalelf0/black-metal-theme-neovim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/saghen/blink.cmp",                  version = vim.version.range("^1") },
 })
 
+-- Colorscheme
 vim.cmd("colorscheme darkthrone")
 
+-- LSP
 require("mason").setup()
+
+require('blink.cmp').setup({
+	fuzzy = { implementation = "prefer_rust_with_warning" },
+	keymap = { preset = "enter" },
+	appearance = {
+		nerd_font_variant = "mono",
+	},
+	completion = {
+		list = {
+			selection = {
+				preselect = true,
+				auto_insert = false,
+			},
+		},
+		documentation = {
+			auto_show = false,
+			auto_show_delay_ms = 0,
+		},
+	},
+	cmdline = {
+		enabled = false,
+	},
+	signature = {
+		enabled = true,
+	},
+	-- snippets = { preset = "mini_snippets" },
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+
+})
