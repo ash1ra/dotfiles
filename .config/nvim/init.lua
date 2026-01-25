cd69812e77ed21abae9058fc64780b6164fd6a7b
ash1ra <ash1rawtf@gmail.com>
2026-01-25 14:56:18 +0200

    feat (nvim): add peek plugin; add transparency settings to `init.lua`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,23 @@ require("config.options")
 require("config.keymaps")
 require("config.plugins")
 require("config.lsp")
+
+local highlights = {
+	"Normal",
+	"NormalFloat",
+	"NormalNC",
+	"SignColumn",
+	"StatusLine",
+	"StatusLineNC",
+	"WinBar",
+	"WinBarNC",
+	"Folded",
+	"EndOfBuffer",
+	"LineNr",
+	"CursorLineNr",
+	"CursorLine",
+}
+
+for _, group in ipairs(highlights) do
+	vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+end
